895997c768b0dc76500f3751429fefe89df7beab
Jordan Moldow <jmoldow@mit.edu>
2014-05-11 19:41:00 -0400

    Fixes bug with IDP setup
    Unfortunately, unless we hard-code the list of IDP hosts in the package,
    we must initialize the IDP in the application, before the accounts-idp
    package startup scripts run.

--- idp_config.js
+++ idp_config.js
@@ -6,3 +6,10 @@ MYLAR_IDP_PUB = '8a7fe03431b5fc2db3923a2ab6d1a5ddf35cd64aea35e743' +
                 'ded7655f0dc7e085858eeec06e1c7da58c509d57da56dbe6';
 MYLAR_IDP_HOSTS = ["http://localhost:3000"]
 
+// Choose a random IDP server to use.
+// MYLAR_IDP_HOSTS and MYLAR_IDP_PUB must be defined in an application as
+// package-scope variables (variables with no 'var' declaration).
+// MYLAR_IDP_HOSTS  - array of strings, hosts of the IDP servers
+// MYLAR_IDP_PUB    - string, the common public key of all the IDP servers
+MYLAR_CHOSEN_IDP_HOST = Random.choice(MYLAR_IDP_HOSTS);
+idp_init(MYLAR_CHOSEN_IDP_HOST, MYLAR_IDP_PUB, false);
--- packages/mylar-mail/mylar_mail_common.js
+++ packages/mylar-mail/mylar_mail_common.js
@@ -1,17 +1,3 @@
 /*
  * mylar_mail_common.js - Common code for the mylar-mail Meteor package.
  */
-
-MYLAR_CHOSEN_IDP_HOST = "";
-
-Meteor.startup(function () {
-
-  // On start-up, choose a random IDP server to use.
-  // MYLAR_IDP_HOSTS and MYLAR_IDP_PUB must be defined in an application as
-  // package-scope variables (variables with no 'var' declaration).
-  // MYLAR_IDP_HOSTS  - array of strings, hosts of the IDP servers
-  // MYLAR_IDP_PUB    - string, the common public key of all the IDP servers
-  MYLAR_CHOSEN_IDP_HOST = Random.choice(MYLAR_IDP_HOSTS);
-  idp_init(MYLAR_CHOSEN_IDP_HOST, MYLAR_IDP_PUB, false);
-
-});
