f47d06230dbb30ebeb65c00bd132c7020993a71f
Jordan Moldow <jmoldow@mit.edu>
2014-05-11 23:03:40 -0400

    Removes more cruft

--- idp_config.js
+++ idp_config.js
@@ -2,6 +2,11 @@
  * idp_config.js - Application configuration for IDP servers.
  */
 
+MYLAR_ACTIVE_ATTACKER = true;
+// use IDP only if active attacker
+Accounts.config({sendVerificationEmail:active_attacker(),
+                 loginExpirationInDays:1});
+
 MYLAR_IDP_PUB = '8a7fe03431b5fc2db3923a2ab6d1a5ddf35cd64aea35e743' +
                 'ded7655f0dc7e085858eeec06e1c7da58c509d57da56dbe6';
 MYLAR_IDP_HOSTS = ["http://localhost:3000"]
